74a9b334ae9ca393ed11bae5dbbc532ca8acde74
Five-Character Code <39875581+1stchrc@users.noreply.github.com>
2020-11-01 22:02:41 +0800

    Add files via upload

---
+++ scripts/framework/RectCollisionDetection.js
@@ -0,0 +1,59 @@
+function RectCollider(left,bottom,right,top,obj = null){
+    this.left = left;
+    this.bottom = bottom;
+    this.right = right;
+    this.top = top;
+    this.obj = obj;
+}
+function GridLayer(sizeX,sizeY,scaleX,scaleY){
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    this.scaleX = scaleX;
+    this.scaleY = scaleY;
+    this.gridList = [];
+    this.gridList.length = this.sizeX*this.sizeY;
+    this.addRect = function(rect){
+        var xmin = Math.floor(rect.left/this.scaleX);
+        var xmax = Math.ceil(rect.right/this.scaleX)
+        var ymax = Math.ceil(rect.top/this.scaleY)
+        for(var y = Math.floor(rect.bottom/this.scaleY); y < ymax; y++){
+            for(var x = xmin; x < xmax; x++){
+                this.gridList[x + y * this.sizeX].push(rect);
+            }
+        }
+    }
+    this.removeRect = function(rect){
+        var xmin = Math.floor(rect.left/this.scaleX);
+        var xmax = Math.ceil(rect.right/this.scaleX)
+        var ymax = Math.ceil(rect.top/this.scaleY)
+        for(var y = Math.floor(rect.bottom/this.scaleY); y < ymax; y++){
+            for(var x = xmin; x < xmax; x++){
+                this.gridList[x + y * this.sizeX](x,y).splice(set.indexOf(rect),1);
+            }
+        }
+    }
+    this.overlapRect = function(l,b,r,t){
+        var array = [];
+        var xmin = Math.floor(l/this.scaleX);
+        var xmax = Math.ceil(r/this.scaleX)
+        var ymax = Math.ceil(b/this.scaleY)
+        for(var y = Math.floor(t/this.scaleY); y < ymax; y++){
+            for(var x = xmin; x < xmax; x++){
+                this.gridList[x + y * this.sizeX].forEach(r=>{
+                    if(array.indexOf(rect) != -1)
+                        return;
+                    if(r.left > r)
+                        return;
+                    if(l > r.right)
+                        return;
+                    if(r.bottom > t)
+                        return;
+                    if(b > r.top)
+                        return;
+                    array.push(r);
+                });
+            }
+        }
+        return array;
+    }
+}
